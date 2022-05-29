803445c1532eecbe42dac2874916bb15254dd22f
Tanya Kumari <95133237+tanya17-05@users.noreply.github.com>
2022-05-29 15:14:00 +0530

    Add files via upload

---
+++ src/SquareComponent.jsx
@@ -0,0 +1,10 @@
+const SquareComponent = (props) => {
+    const classes = props.className?`${props.className} square` : "square";
+  return (
+    <span className={classes} onClick={props.onClick} >
+        {props.state}
+    </span>
+  )
+}
+
+export default SquareComponent
